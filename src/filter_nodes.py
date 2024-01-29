c773f81f7eb60adc52456c87777791cc5801faf4
cbizon <bizon@renci.org>
2024-01-29 15:21:51 -0500

    filter a nodefile based on an edgefile

---
+++ src/filter_nodes.py
@@ -0,0 +1,19 @@
+import sys
+import jsonlines
+
+def go(edge_file, node_in, node_out):
+    keepnodes = set()
+    with jsonlines.open(edge_file, 'r') as inf:
+        for line in inf:
+            keepnodes.add(line["subject"])
+            keepnodes.add(line["object"])
+    with jsonlines.open(node_in, 'r') as inf, jsonlines.open(node_out, 'w') as outf:
+        for line in inf:
+            if line["id"] in keepnodes:
+                outf.dumps(line)
+
+if __name__ == "__main__":
+    edge_file = sys.argv[1]
+    node_in = sys.argv[2]
+    node_out = sys.argv[3]
+    go(edge_file, node_in, node_out)
